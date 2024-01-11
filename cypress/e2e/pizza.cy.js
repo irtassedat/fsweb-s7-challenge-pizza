4c18c054adc050a2eb107eb8bd723e3641d3d906
irtassedat <sedatirtas.1@gmail.com>
2024-01-11 07:18:08 +0300

    Cypress test dosyası eklendi düzenlemeler yapılacak ;

---
+++ cypress/e2e/pizza.cy.js
@@ -0,0 +1,13 @@
+describe("Ana Sayfa", () => {
+    it("Ana sayfa açılmalı ve siparişe git butonu bulunmalıdır", () => {
+      cy.visit("/");
+      cy.get("#order-pizza").should("exist");
+    });
+  
+    it("Siparişe Git butonuna tıklanmalıdır ve pizza formu açılmalıdır", () => {
+      cy.visit("/");
+      cy.get("#order-pizza").click();
+      cy.url().should("include", "/pizza");
+    });
+  });
+  
